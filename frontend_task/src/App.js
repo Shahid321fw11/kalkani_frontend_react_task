d63f4936d2102850ccfacb944f3ecb4733a4f9bf
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-06-16 04:18:56 +0530

    Screen1 done

--- frontend_task/src/App.js
+++ frontend_task/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import { Routes, Route } from 'react-router-dom';
+import Screen1 from './Components/Screen1';
 
 function App() {
   return <>
     <Navbar />
     <Routes>
-      <Route path="/screen1" element={<h1>Home</h1>} />
+      <Route path="/screen1" element={<Screen1 />} />
       <Route path="/screen2" element={<h1>About</h1>} />
     </Routes>
   </>
--- frontend_task/src/Components/Screen1.jsx
+++ frontend_task/src/Components/Screen1.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Box from '@mui/material/Box';
+
+
 
 const Screen1 = () => {
     return <>
-        <h1>Screen 1</h1>
+        <Paper elevation={3} style={{ maxWidth: '80%', marginLeft: 'auto', marginRight: 'auto', paddingBottom: '1rem' }}>
+            <Box
+                sx={{
+                    width: '80%',
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    padding: '1rem',
+                }}
+            >
+
+                <TextField
+                    fullWidth
+                    id="filled-multiline-static"
+                    // label="Multiline"
+                    placeholder="Paste your json here..."
+                    multiline
+                    rows={14}
+                    variant="filled"
+                    style={{ display: 'block', minWidth: '5rem', }}
+                />
+            </Box>
+
+
+            <Button variant="outlined" style={{ marginTop: '1rem', marginLeft: 'auto', marginRight: 'auto', display: 'block', width: '80%', marginBottom: '1rem' }}> Create Form</Button>
+        </Paper>
+
     </>
 }
 
